74e7950c613986fb90052b7c9f1f6ac15cb9bd5e
kqian18 <141068344+kqian18@users.noreply.github.com>
2024-07-15 16:18:19 -0400

    edit AddLabel

--- src/cell_table.cpp
+++ src/cell_table.cpp
@@ -785,7 +785,7 @@ void CellTable::AddColumn(const Tag& tag,
   
 }
 
-void CellTable::AddLabel(const Tag& tag, StringColPtr value) {
+void CellTable::AddLabel(const Tag& tag, StringPtr value) {
     if (value->size() != CellCount()) {
         throw std::runtime_error("Adding column of incorrect size");
     }
